d36ed3f35cde629ce654f8d7300118203750d453
Todd Holforty <tholforty@gmail.com>
2018-09-09 23:32:13 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# composer-example-app
+======================
+
+This is a simple app facilitated with composer to demonstrate basic function and tests. 
---
+++ src/HelloWorld/Greetings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HelloWorld;
+
+use SebastianBergmann\Timer\Timer;
+
+class Greetings {
+  public static function sayHelloWorld() {
+    $timer = new Timer();
+    $timer->start();
+    return "Hello World\n" . $timer->resourceUsage() . "\n";
+  }
+}
